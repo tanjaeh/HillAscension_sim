76d0cac7531e0763718aa2a577527588db15eb3d
tanjaeh <tanjaehenriksen@gmail.com>
2022-03-06 21:12:22 +0100

    started to create model

---
+++ Tire.py
@@ -0,0 +1,9 @@
+class Tire:
+    def __init__(self, mass, radius, width, pos):
+        self.mass = mass
+        self.radius = radius
+        self.width = width
+        self.pos = pos
+
+
+#Create tire function
